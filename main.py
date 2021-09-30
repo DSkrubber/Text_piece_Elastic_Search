4253206602ea835851144f68ded965582ec0e012
Dmitrii Chebotarev <skrubberqumo@gmail.com>
2021-09-30 12:05:20 +0300

    Connects web main application to ElasticSearch container.
    - send and find data with help of elasticsearch python library.
    - add 2 endpoints for app:
        * /send with PUT method that sends TextPiece data to the ElasticSearch;
        * /load with GET method that searches TextPieces with correct pattern in ElasticSearch storage;
    - add 3 examples for SWAGGER (FastAPI /docs endpoint) with different valid TextPieces and failure case via examples dict in main.py;
    - query pattern via "query" global variable in main.py;
    - add elasticsearch async in requirements.txt;
    - change python image tag in Dockerfile to "slim".

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from json import loads
+from datetime import datetime
 from typing import Optional
-from uuid import uuid4
+from uuid import UUID, uuid4
 
-from fastapi import FastAPI
+from elasticsearch import Elasticsearch
+from fastapi import Body, FastAPI
 from pydantic import BaseModel, validator
 
-DB = {}
+es = Elasticsearch(hosts="elasticsearch:9200")
+# es = Elasticsearch(hosts="localhost:9200")
 app = FastAPI()
 
 
 class TextPiece(BaseModel):
     text: str = "Some default text for testing functionality."
-    piece_id: Optional[str] = str(uuid4())
+    piece_id: Optional[UUID] = uuid4()
     type: str
     page_number: int
     document_name: str
+    timestamp: Optional[datetime] = datetime.now()
 
     @validator("type")
     def possible_content_type(cls, value: str):
@@ -29,18 +32,48 @@ class TextPiece(BaseModel):
         return name
 
 
-@app.get("/")
-async def index():
-    return {"Hello": "All"}
+examples = {
+    "passing_1": {
+        "summary": "First normal example",
+        "value": {
+            "text": "Some default text for testing functionality.",
+            "type": "title",
+            "page_number": 0,
+            "document_name": "doc.pdf",
+        },
+    },
+    "passing_2": {
+        "summary": "Second normal example",
+        "value": {
+            "text": "Another text for testing functionality.",
+            "piece_id": "12345d1d-5fca-4ae2-a4d4-fa1d1ce0d111",
+            "type": "paragraph",
+            "page_number": 1,
+            "document_name": "another_doc.pdf",
+        },
+    },
+    "invalid": {
+        "summary": "Invalid values example",
+        "value": {
+            "text": "This will be failure case.",
+            "type": "wrong type",
+            "page_number": 2,
+            "document_name": "doc.notpdf",
+        },
+    },
+}
+
+query = {"match": {"text": "Some"}}
 
 
-@app.get("/pieces/{piece_id}")
-async def read_root(piece_id: str):
-    return loads(DB[piece_id].json())
+@app.put("/send")
+async def add_item(*, item: TextPiece = Body(..., examples=examples)):
+    doc = item.json()
+    res = es.index(index="job_id", id=item.piece_id, document=doc)
+    return res
 
 
-@app.put("/pieces")
-async def update_item(piece: TextPiece):
-    global DB
-    DB[piece.piece_id] = piece
-    return loads(piece.json())
+@app.get("/load")
+def get_item():
+    res = es.search(index="job_id", _source=False, query=query)
+    return res
