6d66761aebe09d409114312c6f8ffb32941c37e0
Dmitrii Chebotarev <skrubberqumo@gmail.com>
2022-05-05 21:34:16 +0300

    feat(web): add modules for postgresql connection and management and sqlalchemy models definition

---
+++ web/database.py
@@ -0,0 +1,33 @@
+from typing import Iterator
+
+from dotenv import find_dotenv, load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .constants import (
+    POSTGRES_DB,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+)
+
+load_dotenv(find_dotenv("../.env"))
+
+SQLALCHEMY_POSTGRESQL_URL = "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
+    POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB
+)
+engine = create_engine(SQLALCHEMY_POSTGRESQL_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db() -> Iterator[sessionmaker]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ web/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import CheckConstraint, Column, ForeignKey, func
+from sqlalchemy.dialects.postgresql import (
+    BOOLEAN,
+    ENUM,
+    INTEGER,
+    JSONB,
+    TEXT,
+    VARCHAR,
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import TIMESTAMP
+
+from .database import Base
+from .schemas import TextPieceType
+
+
+class Document(Base):  # type: ignore
+    __tablename__ = "documents"
+
+    name = Column(VARCHAR, primary_key=True)
+    author = Column(VARCHAR, nullable=False, index=True)
+    text_pieces = relationship(
+        "TextPiece",
+        back_populates="document",
+        cascade="all, delete, delete-orphan",
+    )
+
+
+class TextPiece(Base):  # type: ignore
+    __tablename__ = "text_pieces"
+
+    piece_id = Column(INTEGER, primary_key=True)
+    meta_data = Column(JSONB, nullable=True)
+    indexed = Column(BOOLEAN, nullable=False, default=False)
+    document_name = Column(
+        VARCHAR, ForeignKey("documents.name"), nullable=False, index=True
+    )
+    size = Column(INTEGER, nullable=False, index=True)
+    type = Column(ENUM(TextPieceType), nullable=False)
+    page = Column(
+        INTEGER,
+        CheckConstraint("page > 1", name="positive_page"),
+        nullable=False,
+    )
+    text = Column(TEXT, nullable=False)
+    created_at = Column(
+        TIMESTAMP, server_default=func.now(), nullable=False, index=True
+    )
+    document = relationship("Document", back_populates="text_pieces")
