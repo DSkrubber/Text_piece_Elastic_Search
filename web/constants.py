ec87fe7624c5dbfbb2d6591d64b058188fbdc209
Dmitrii Chebotarev <skrubberqumo@gmail.com>
2022-05-06 22:01:15 +0300

    feat(web): add ES client connection, index creation logic, define ES settings

--- web/constants.py
+++ web/constants.py
@@ -5,8 +5,11 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 
-APP_HOST = os.environ.get("APP_HOST")
-APP_PORT = os.environ.get("APP_PORT")
+APP_HOST = os.environ.get("APP_HOST", "localhost")
+APP_PORT = os.environ.get("APP_PORT", 9200)
+
+ES_HOST = os.environ.get("ES_HOST")
+ES_PORT = os.environ.get("ES_PORT")
 
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "admin")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "admin")
