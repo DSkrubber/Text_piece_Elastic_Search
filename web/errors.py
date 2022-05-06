bae41fbef79b01d261ab58a9f84a079b0e925522
Dmitrii Chebotarev <skrubberqumo@gmail.com>
2022-05-06 22:01:29 +0300

    feat(web): add ES connection open-close on app startup-shutdown, index creation on document POST and elasticsearch exception handlers

--- web/errors.py
+++ web/errors.py
@@ -1,3 +1,4 @@
+from elasticsearch.exceptions import ApiError, RequestError, TransportError
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 from sqlalchemy.exc import DBAPIError, SQLAlchemyError
@@ -22,4 +23,34 @@ def dbapi_exception_handler(
     return JSONResponse(
         status_code=500,
         content={"detail": f"Error: DBAPI error ({error.__cause__})"},
+    )
+
+
+def es_transport_exception_handler(
+    request: Request, error: TransportError
+) -> JSONResponse:
+    database_logger.error(error)
+    return JSONResponse(
+        status_code=500,
+        content={"detail": f"Error: ElasticSearch transport error ({error})"},
+    )
+
+
+def es_api_exception_handler(
+    request: Request, error: ApiError
+) -> JSONResponse:
+    database_logger.error(error)
+    return JSONResponse(
+        status_code=500,
+        content={"detail": f"Error: ElasticSearch API error ({error})"},
+    )
+
+
+def es_request_exception_handler(
+    request: Request, error: RequestError
+) -> JSONResponse:
+    database_logger.error(error)
+    return JSONResponse(
+        status_code=500,
+        content={"detail": f"Error: ElasticSearch Request error ({error})"},
     )
